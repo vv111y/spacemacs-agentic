3a07dbf86566d666c574b57bd9ddbd6877882fc2
vv111y <vv111y@users.noreply.github.com>
2026-07-10 16:35:56 -0400

    Declare layer dependencies

---
+++ layers.el
@@ -0,0 +1,8 @@
+;;; layers.el --- Agentic systems layer dependencies -*- lexical-binding: nil; -*-
+
+;; Copyright (C) 2026 Willy Rempel
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+(configuration-layer/declare-layer-dependencies '(git org))
+
+;;; layers.el ends here
